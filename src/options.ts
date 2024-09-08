ff755375f5558375ca66cd61f71eabbee063fe64
Axel <nigthmaresans2@gmail.com>
2024-09-08 12:21:59 -0600

    added: configs for upcoming weather service

--- src/options.ts
+++ src/options.ts
@@ -3,4 +3,9 @@ import { opt } from "./lib/config.js";
 export default {
   wallpaper: opt("wallpaper"),
   provider: opt("provider", true),
+  location: {
+    city_name: opt("current-city-name"),
+    lat: opt("latitude"),
+    long: opt("longitude"),
+  },
 };
