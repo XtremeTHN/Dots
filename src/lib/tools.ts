d15713caf40132cf527ca24136db61959dc6c509
Axel <nigthmaresans2@gmail.com>
2024-09-08 14:51:50 -0600

    added: which and fileExists to tools.ts

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -1,1 +1,10 @@
-export const toTitle = (/** @type {string | any[]} */ word) => `${word.at(0).toUpperCase()}${word.slice(1)}`
+import GLib from "gi://GLib";
+
+export const toTitle = (word: string) =>
+  `${word.at(0)?.toUpperCase()}${word.slice(1)}`;
+
+export const which = (program) =>
+  Utils.exec(["bash", "-c", `which ${program} &> /dev/null; echo $status`]) !==
+  "1";
+
+export const fileExists = (file) => GLib.file_test(file, GLib.FileTest.EXISTS);
