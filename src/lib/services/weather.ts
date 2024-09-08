789e5f904e8c85744b2d45c89e37baccbdd986b1
Axel <nigthmaresans2@gmail.com>
2024-09-08 12:22:17 -0600

    testing: weather service

--- src/lib/services/weather.ts
+++ src/lib/services/weather.ts
@@ -1,8 +1,46 @@
 import GWeather from "gi://GWeather";
+import Geoclue from "gi://Geoclue";
 
-// const location = GWeather.Location.new_detached(
-// PERSONAL DATA xd
+// import options from "src/options";
+
+// let location = GWeather.Location.new_detached(
+//   "Guadalupe",
+//   null,
+//   25.67678,
+//   -100.25646,
 // );
+
+// const get_level = (loc: GWeather.Location, level: GWeather.LocationLevel) => {
+//   let l = loc.get_level();
+//   let location = loc;
+
+//   if (l < level) {
+//     location.find_nearest_city(25.58333, -99.75);
+//   } else if (l > level) {
+//     let le = l;
+//     while (le > level) {
+//       location = location.get_parent();
+//       le = location.get_level();
+//     }
+//   }
+
+//   return location;
+// };
+
+// const getCity = (loc) => get_level(loc, GWeather.LocationLevel.CITY);
+// const getState = (loc) => get_level(loc, GWeather.LocationLevel.ADM1);
+
+// let state = getState(location).get_name();
+// let city = getCity(location).get_name();
+
+// console.log(city, state);
+
+// let city_obj = getCity(location);
+
+// console.log("asd", location.get_parent()?.get_parent()?.get_name());
+
+// console.log(city, state);
+
 // const info = new GWeather.Info({
 //   applicationId: "com.github.Aylur.ags",
 //   contactInfo: "xtreme.dev2@gmail.com",
@@ -20,8 +58,29 @@ import GWeather from "gi://GWeather";
 // info.update();
 // console.log("updating");
 
-class weatherapi extends Service {
-  static {
-    Service.register(this, {});
-  }
-}
+const get_location_geoclue = () => {
+  let simple = Geoclue.Simple.new;
+};
+
+// class weatherapi extends Service {
+//   static {
+//     Service.register(
+//       this,
+//       {},
+//       {
+//         temperature: ["float", "rw"],
+//         city_name: ["string", "rw"],
+//         state_name: ["string", "rw"],
+//         location: ["gobject", "rw"],
+//       },
+//     );
+//   }
+
+//   constructor() {
+//     super();
+
+//     if (options.location.lat.value == 0 && options.location.long.value == 0) {
+//       console.debug("Using geolocation");
+//     }
+//   }
+// }
